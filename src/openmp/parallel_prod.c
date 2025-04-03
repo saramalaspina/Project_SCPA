c4dc983f02329314b9caba4dc1f6a49c12730bd4
saramalaspina <saramalaspina2001@gmail.com>
2025-04-03 18:03:52 +0200

    scheduling guided hll openmp

--- src/openmp/parallel_prod.c
+++ src/openmp/parallel_prod.c
@@ -4,26 +4,6 @@
 #include <stdlib.h>
 #include <omp.h>
 
-/*void prodOpenmpCSR(int M, CSRMatrix *csr, double *x, double *y) {
-    int chunk_size = (M > 10000) ? 256 : 8;
-
-    #pragma omp parallel for schedule(dynamic, chunk_size)
-    for (int i = 0; i < M; i++) {  // Scorre le righe della matrice
-        double sum = 0.0;  // Variabile privata per evitare race conditions
-
-        int start = csr->IRP[i];
-        int end = csr->IRP[i + 1];
-
-        for (int j = start; j < end; j++) {  // Scorre elementi non nulli della riga i
-            sum += csr->AS[j] * x[csr->JA[j]];
-        }
-
-        y[i] = sum;
-    }
-}*/
-
-
-
 void prodOpenmpCSR(int M, CSRMatrix *csr, double *x, double *y, int *row_bounds) {
     int num_threads = omp_get_max_threads();
 
@@ -45,10 +25,9 @@ void prodOpenmpCSR(int M, CSRMatrix *csr, double *x, double *y, int *row_bounds)
 
 
 void prodOpenmpHLL(HLLMatrix *hll, double *x, double *y) {
-    int chunk_size = (hll->numBlocks > 500) ? 8 : (hll->numBlocks > 100) ? 4 : 2;
-    
+
     /* Parallelizzazione sul ciclo esterno (sui blocchi) */
-    #pragma omp parallel for schedule(dynamic, chunk_size)
+    #pragma omp parallel for schedule(guided)
     for (int b = 0; b < hll->numBlocks; b++) {
         EllpackBlock *block = &hll->blocks[b];
         /* Il blocco b inizia dalla riga globale base = b * hackSize */
